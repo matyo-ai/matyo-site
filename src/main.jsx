7d33931431bf6ef5d4126ad9ca11446037a88958
Matthew Jones <matthew@MBP-Matthew.lan>
2026-06-17 19:01:19 +0300

    init

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import ArtistSite from "./artist-site.jsx"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ArtistSite />
+)
+
+
